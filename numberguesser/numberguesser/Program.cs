f72ff5367ac0b7bd03b79b3e4279e7ce09238e3f
Liquid-610 <jamesbarnes71@gmail.com>
2014-03-05 10:12:24 -0500

    start'

--- numberguesser/numberguesser/Program.cs
+++ numberguesser/numberguesser/Program.cs
@@ -9,6 +9,46 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            String input;
+
+            int guess = 50;
+            int guess2 = guess / 2;
+            int guessh = guess2 + guess;
+            int guessl = guess - guess2;
+
+
+
+
+            while (guess == 50)
+            {
+                guess = 100 / 2;
+                guess = (100 / 2) + guess2;
+
+
+                Console.WriteLine("Is your number higher(h) , lower(l) or equal(e) to 50?");
+
+                input = Console.ReadLine();
+
+                if (input == "h")
+                {
+                    Console.WriteLine("{0}", guessh);
+
+                }
+                else if (input == "l")
+                {
+                Console.WriteLine("{0}", guessl);
+                }
+
+                else if (input =="e")
+
+                    Console.WriteLine("I Win!!!!");
+                
+                
+
+                
+                }
+           }
+
         }
     }
-}
+
